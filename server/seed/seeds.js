5bd6b0416a91ee43a697b7cd30758d3a82db3949
Amelia Bigler <amaebigler@gmail.com>
2023-05-15 21:33:12 -0500

    add ingredient unit and steps to recipe model. add recipe seeds.

--- server/seed/seeds.js
+++ server/seed/seeds.js
@@ -1,5 +1,5 @@
 const db = require('../config/connection');
-const { Recipes, User, Ingredient, Comment } = require('../models');
+const { Recipes, User } = require('../models');
 const recipeSeeds = require('./recipeSeeds.json');
 const userSeeds = require('./userSeeds.json');
 
@@ -10,12 +10,6 @@ db.once('open', async () => {
 
     await User.deleteMany({});
     await User.create(userSeeds);
-
-    await Ingredient.deleteMany({});
-    await Ingredient.create(ingredientSeeds);
-
-    await Comment.deleteMany({});
-    await Comment.create(commentSeeds);
 
     console.log('seed done');
     process.exit(0);
