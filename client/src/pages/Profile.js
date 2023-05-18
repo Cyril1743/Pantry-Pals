d480ad3a5022504d1b950dab92e086d1bbdc2323
Amelia Bigler <amaebigler@gmail.com>
2023-05-18 10:42:25 -0500

    add profile query and profile route to client side.

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,15 +1,42 @@
 import React from 'react'
 import { Container } from '@chakra-ui/react'
-import { useParams } from 'react-router-dom'
+import { Navigate, useParams } from 'react-router-dom'
+import { useQuery } from '@apollo/client'
+
+import { QUERY_USER, QUERY_ME } from '../utils/queries';
+
+import Auth from '../utils/auth';
 
 export default function Profile() {
-    //TODO: Use the useQuery hook to get profile info
 
     const { username } = useParams()
 
+    const { data } = useQuery(
+        username ? QUERY_USER : QUERY_ME,
+        {
+            variables: { username: username },
+        }
+    );
+    
+    console.log(data)
+    const profile = data?.me || data?.user || {};
+    console.log(profile)
+
+    if (Auth.loggedIn() && Auth.getUser().data.username === username) {
+        return <Navigate to="/profile/me" />;
+    }
+
+    if (!profile?.username) {
+        return (
+          <h4>
+            Log in to see your profile page.
+          </h4>
+        );
+    }
+
     return (
         <Container>
-            Nothing here for now
+            <h2>{username ? `${profile.name}'s` : 'Your'} Profile</h2>
         </Container>
     )
 }
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,9 +1,23 @@
 import { gql } from '@apollo/client';
 
+export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      recipe
+      favorites
+    }
+  }
+`
+
 export const QUERY_USER = gql`
   query user($username: String!) {
     user(username: $username) {
+      _id
       username
+      recipe
+      favorites
     }
   }
 `
@@ -72,7 +86,5 @@ query recipe($recipeId: ID!) {
       username
     }
   }
-}`
-
 }`;
 
