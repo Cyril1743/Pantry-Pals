afe1fb5fe9392bc3aff2b4f85c0d2c6b0071f128
Amelia Bigler <amaebigler@gmail.com>
2023-05-19 16:56:22 -0500

    add recipe form input fields and buttons.

--- client/src/components/RecipeForm.js
+++ client/src/components/RecipeForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Container, FormControl, Input, FormHelperText, FormErrorMessage, Alert, AlertTitle, AlertDescription, FormLabel, Button } from "@chakra-ui/react";
+import { Box, Container, FormControl, Input, FormHelperText, FormErrorMessage, Alert, AlertTitle, AlertDescription, FormLabel, Button, NumberInput, NumberInputField, Stack } from "@chakra-ui/react";
 import { useMutation } from '@apollo/client';
 
 import { ADD_RECIPE } from '../../utils/mutations';
@@ -8,51 +8,111 @@ import Auth from '../../utils/auth';
 
 const RecipeForm = () => {
 
-  //States to store all the logic
-  const [name, setName] = useState('');
-  const [description, setDescription] = useState('');
-  const [servings, setServings] = useState('');
-  const [ingredientName, setIngredientName] = useState('');
-  const [ingredientAmount, setIngredientAmount] = useState('');
-  const [ingredientUnit, setIngredientUnit] = useState('');
-  const [stepOrder, setStepOrder] = useState('');
-  const [stepText, setStepText] = useState('');
+    //States to store all the logic
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+    const [servings, setServings] = useState('');
+    const [ingredientName, setIngredientName] = useState('');
+    const [ingredientAmount, setIngredientAmount] = useState('');
+    const [ingredientUnit, setIngredientUnit] = useState('');
+    const [stepOrder, setStepOrder] = useState('');
+    const [stepText, setStepText] = useState('');
 
 
-  const [addRecipe, { error }] = useMutation(ADD_RECIPE);
+    const [addRecipe, { error }] = useMutation(ADD_RECIPE);
 
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
+    const handleAddIngredient = () => {}
 
-    try {
-      const data = await addRecipe({
-        variables: { name, description, servings, ingredientName, ingredientAmount, ingredientUnit, stepOrder, stepText },
-      });
+    const handleAddStep = () => {}
 
-      setName('');
-      setDescription('');
-      setServings('')
-      setIngredientName('');
-      setIngredientAmount('');
-      setIngredientUnit('');
-      setStepOrder('');
-      setStepText('');
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
 
-    } catch (err) {
-      console.error(err);
-    }
-  };
+        try {
+            const data = await addRecipe({
+                variables: { name, description, servings, ingredientName, ingredientAmount, ingredientUnit, stepOrder, stepText },
+            });
 
-  return (
-    
-    <Container className="recipeContainer">
-        {Auth.loggedIn() ? (
-            <h2>The form will go here</h2>
-        ) : (
-            <h2>Oops, you need to log in to create a recipe!</h2>
-        )} 
-    </Container>
-)
+            setName('');
+            setDescription('');
+            setServings('')
+            setIngredientName('');
+            setIngredientAmount('');
+            setIngredientUnit('');
+            setStepOrder('');
+            setStepText('');
+
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    return (
+
+        <Container className="recipeFormContainer">
+            {Auth.loggedIn() ? (
+                <React.Fragment>
+                    <FormControl isRequired>
+                        <FormLabel>Recipe name</FormLabel>
+                        <Input placeholder='Recipe name' />
+                    </FormControl>
+                    <FormControl isRequired>
+                        <FormLabel>Description</FormLabel>
+                        <Input placeholder='Short recipe description' />
+                    </FormControl>
+                    <FormControl isRequired>
+                        <FormLabel>Servings</FormLabel>
+                        <NumberInput min={1}>
+                            <NumberInputField />
+                        </NumberInput>
+                    </FormControl>
+
+                    <Stack direction='column'>
+                        <Box>
+                            <FormControl isRequired>
+                                <FormLabel>Ingredient Name</FormLabel>
+                                <Input placeholder='Ingredient Name' />
+                            </FormControl>
+                            <FormControl isRequired>
+                                <FormLabel>Ingredient Amount</FormLabel>
+                                <Input placeholder='Ingredient Amount' />
+                            </FormControl>
+                            <FormControl>
+                                <FormLabel>Units</FormLabel>
+                                <Input />
+                                <FormHelperText>
+                                    Leave blank if units are not applicable to this ingredient
+                                </FormHelperText>
+                            </FormControl>
+                        </Box>
+                        <Button onClick={handleAddIngredient}>
+                            <span role='img' aria-label='add'>
+                                &#10133;
+                            </span>
+                        </Button>
+                    </Stack>
+
+
+                    <Stack direction='column'>
+                        <FormControl isRequired>
+                            <FormLabel>Step</FormLabel>
+                            <Input />
+                        </FormControl>
+                        <Button onClick={handleAddStep}>
+                            <span role='img' aria-label='add'>
+                                &#10133;
+                            </span>
+                        </Button>
+                    </Stack>
+
+                    <Button onClick={handleFormSubmit}>Publish</Button>
+
+                </React.Fragment>
+            ) : (
+                <h2>Oops, you need to log in to create a recipe!</h2>
+            )}
+        </Container>
+    )
 };
 
 export default RecipeForm;
