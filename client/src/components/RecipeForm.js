19babb720d670bf9834e8682af76ef8c543a9171
Amelia Bigler <amaebigler@gmail.com>
2023-05-20 15:26:25 -0500

    add recipe form react.

--- client/src/components/RecipeForm.js
+++ client/src/components/RecipeForm.js
@@ -1,45 +1,62 @@
 import React, { useState } from 'react';
-import { Box, Container, FormControl, Input, FormHelperText, FormErrorMessage, Alert, AlertTitle, AlertDescription, FormLabel, Button, NumberInput, NumberInputField, Stack } from "@chakra-ui/react";
+import { Box, Container, FormControl, Input, FormHelperText, UnorderedList, OrderedList, ListItem, FormErrorMessage, Alert, AlertTitle, AlertDescription, FormLabel, Button, NumberInput, NumberInputField, Stack } from "@chakra-ui/react";
 import { useMutation } from '@apollo/client';
 
-import { ADD_RECIPE } from '../../utils/mutations';
+import { ADD_RECIPE } from '../utils/mutations';
 
-import Auth from '../../utils/auth';
+import Auth from '../utils/auth';
 
-const RecipeForm = () => {
+export default function RecipeForm() {
 
     //States to store all the logic
     const [name, setName] = useState('');
     const [description, setDescription] = useState('');
     const [servings, setServings] = useState('');
-    const [ingredientName, setIngredientName] = useState('');
+    const [ingredientName, setIngredientName] = useState('')
     const [ingredientAmount, setIngredientAmount] = useState('');
     const [ingredientUnit, setIngredientUnit] = useState('');
-    const [stepOrder, setStepOrder] = useState('');
+    const [ingredients, setIngredients] = useState([]);
+    const [steps, setSteps] = useState([]);
     const [stepText, setStepText] = useState('');
-
 
     const [addRecipe, { error }] = useMutation(ADD_RECIPE);
 
-    const handleAddIngredient = () => {}
+    const handleAddIngredient = (e) => {
+        setIngredientName('')
+        setIngredientAmount('')
+        setIngredientUnit('')
+        setIngredients([...ingredients, {ingredientName, ingredientAmount, ingredientUnit}])
+    }
+    
+    const handleAddStep = (e) => {
+        setStepText('')
+        setSteps([...steps, {stepText}])
+    }
 
-    const handleAddStep = () => {}
+    const removeIngrdnt = (ingrdnt) => {
+        setIngredients((prevIngrdnts) => prevIngrdnts.filter((item) => item !== ingrdnt))
+    }
+
+    const removeStep = (step) => {
+        setSteps((prevSteps) => prevSteps.filter((item) => item !== step))
+    }
 
     const handleFormSubmit = async (event) => {
         event.preventDefault();
 
         try {
             const data = await addRecipe({
-                variables: { name, description, servings, ingredientName, ingredientAmount, ingredientUnit, stepOrder, stepText },
+                variables: { name, description, servings, ingredients, steps },
             });
 
             setName('');
             setDescription('');
             setServings('')
-            setIngredientName('');
+            setIngredients([]);
+            setIngredientName('')
             setIngredientAmount('');
             setIngredientUnit('');
-            setStepOrder('');
+            setSteps([]);
             setStepText('');
 
         } catch (err) {
@@ -47,57 +64,113 @@ const RecipeForm = () => {
         }
     };
 
-    return (
+    const nameChange = (e) => {
+        setName(e.target.value)
+    }
 
+    const descriptionChange = (e) => {
+        setDescription(e.target.value)
+    }
+
+    const servingsChange = (e) => {
+        setServings(e.target.value)
+    }
+
+    const ingredientNameChange = (e) => {
+        setIngredientName(e.target.value)
+    }
+
+    const ingredientAmountChange = (e) => {
+        setIngredientAmount(e.target.value)
+    }
+
+    const ingredientUnitChange = (e) => {
+        setIngredientUnit(e.target.value)
+    }
+
+    const stepChange = (e) => {
+        setStepText(e.target.value)
+    }
+    
+    return (
         <Container className="recipeFormContainer">
             {Auth.loggedIn() ? (
                 <React.Fragment>
                     <FormControl isRequired>
                         <FormLabel>Recipe name</FormLabel>
-                        <Input placeholder='Recipe name' />
+                        <Input placeholder='Recipe name' onChange={nameChange} value={name}/>
                     </FormControl>
                     <FormControl isRequired>
                         <FormLabel>Description</FormLabel>
-                        <Input placeholder='Short recipe description' />
+                        <Input placeholder='Short recipe description' onChange={descriptionChange} value={description}/>
                     </FormControl>
                     <FormControl isRequired>
                         <FormLabel>Servings</FormLabel>
                         <NumberInput min={1}>
-                            <NumberInputField />
+                            <NumberInputField onChange={servingsChange} value={servings}/>
                         </NumberInput>
                     </FormControl>
 
                     <Stack direction='column'>
-                        <Box>
+                        <UnorderedList styleType='none'>
+                            {ingredients.map((ingrdnt) => {
+                                return (
+                                    <React.Fragment key={ingrdnt}>
+                                        <ListItem>{ingrdnt.ingredientName} {ingrdnt.ingredientAmount} {ingrdnt.ingredientUnit}</ListItem>
+                                        <Button onClick={() => removeIngrdnt(ingrdnt)}>
+                                            <span role='img' aria-label='delete'>
+                                                ✖️
+                                            </span>
+                                        </Button>
+                                    </React.Fragment>)
+                            })}
+                        </UnorderedList>
+                        <Box className='IngredientBox'>
                             <FormControl isRequired>
                                 <FormLabel>Ingredient Name</FormLabel>
-                                <Input placeholder='Ingredient Name' />
+                                <Input placeholder='Ingredient Name' onChange={ingredientNameChange} value={ingredientName}/>
                             </FormControl>
                             <FormControl isRequired>
                                 <FormLabel>Ingredient Amount</FormLabel>
-                                <Input placeholder='Ingredient Amount' />
+                                <Input placeholder='Ingredient Amount' onChange={ingredientAmountChange} value={ingredientAmount}/>
                             </FormControl>
                             <FormControl>
                                 <FormLabel>Units</FormLabel>
-                                <Input />
+                                <Input onChange={ingredientUnitChange} value={ingredientUnit}/>
                                 <FormHelperText>
                                     Leave blank if units are not applicable to this ingredient
                                 </FormHelperText>
                             </FormControl>
                         </Box>
-                        <Button onClick={handleAddIngredient}>
+                        <Button onClick={handleAddIngredient} >
                             <span role='img' aria-label='add'>
                                 &#10133;
                             </span>
                         </Button>
+                        
                     </Stack>
 
 
                     <Stack direction='column'>
+                        <OrderedList styleType='none'>
+                            {steps.map((step, i) => {
+                                return (
+                                    <React.Fragment key={step}>
+                                        <ListItem>Step {i+1} - {step.stepText}</ListItem>
+                                        <Button onClick={() => removeStep(step)}>
+                                            <span role='img' aria-label='delete'>
+                                                ✖️
+                                            </span>
+                                        </Button>
+                                    </React.Fragment>)
+                            })}
+                        </OrderedList>
+                        <Box className='stepBox'>
                         <FormControl isRequired>
                             <FormLabel>Step</FormLabel>
-                            <Input />
+                            <Input onChange={stepChange} value={stepText}/>
                         </FormControl>
+                        </Box>
                         <Button onClick={handleAddStep}>
                             <span role='img' aria-label='add'>
                                 &#10133;
@@ -105,14 +178,17 @@ const RecipeForm = () => {
                         </Button>
                     </Stack>
 
-                    <Button onClick={handleFormSubmit}>Publish</Button>
+                    <Button mb={10} onClick={handleFormSubmit}>Publish</Button>
 
                 </React.Fragment>
             ) : (
-                <h2>Oops, you need to log in to create a recipe!</h2>
+                <h2>Something went wrong. Please log in to create a recipe.</h2>
             )}
         </Container>
     )
 };
 
-export default RecipeForm;
+// TODO: add these lines to the Profile.js in the pages folder
+// import RecipeForm from '../components/RecipeForm'
+/* <div>{username ? `List of Recipes` : <RecipeForm />}</div> */
+
